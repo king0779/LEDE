6e0ac69a5cf5984a752713d37df278290c0ef601
king0779 <54210035+king0779@users.noreply.github.com>
2024-03-02 09:29:50 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -6,15 +6,14 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.20.1/g' package/base-files/files/bin/config_generate
-sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/package/feeds/packages/ttyd/files/ttyd.config
-mkdir Modem-Support
-pushd Modem-Support
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support .
-popd
-rm -rf ../../package/wwan
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git 5Gmodem https://github.com/king0779/5Gmodem' >>feeds.conf.default
